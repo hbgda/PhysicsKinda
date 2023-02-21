d17782b72a5f5091468f2243dc6b1ec7c39d1e29
Leroy D'eath <leroy.d275@gmail.com>
2023-02-21 03:02:59 +0000

    idk g

--- src/core/physics/engine.rs
+++ src/core/physics/engine.rs
@@ -1,5 +1,51 @@
+use std::{sync::{Mutex, atomic::{AtomicUsize, Ordering}}, collections::HashMap};
+
 use super::entity::*;
 
+static ENTITY_ID_GEN: AtomicUsize = AtomicUsize::new(0);
+
 pub struct Engine<'e> {
-    _entities: Vec<&'e dyn PhysicsEntity>
+    _entities: HashMap<usize, &'e dyn PhysicsEntity>
+}
+
+impl Engine<'_> {
+    pub fn new() -> Self {
+        Engine {
+            _entities: HashMap::new()
+        }
+    }
+}
+
+impl<'e> Engine<'e> {
+    pub fn register_entity<T>(&mut self, entity: &'e mut T)
+    where 
+        T: PhysicsEntity
+    {
+        self._entities.insert(
+            ENTITY_ID_GEN.fetch_add(1, Ordering::SeqCst),
+            entity as &dyn PhysicsEntity
+        );
+    }
+}
+
+impl <'e> Engine<'e> {
+    pub fn update(&mut self) {
+        let mut x = &mut self._entities;
+        for (_, mut entity) in x.iter_mut() {
+            Engine::update_entity(&mut entity);
+        }   
+    }
+
+    fn update_entity(entity: &mut &dyn PhysicsEntity)
+    {
+        // Just basic currently, will eventually be updated to include various physical elements
+        // such as air resistance, gravity, acceleration etc
+        let curr_pos = entity.get_position();
+        let velocity = entity.get_velocity();
+        let updated_pos = (
+            curr_pos.0 + velocity.0,
+            curr_pos.1 + velocity.1
+        );
+        (&mut entity).set_position(updated_pos)
+    }
 }
--- src/core/physics/entity.rs
+++ src/core/physics/entity.rs
@@ -2,6 +2,6 @@ use struct_extension::extendable;
 
 #[extendable]
 struct PhysicsEntity {
-    position: (u32, u32),
-    velocity: (u32, u32)
+    position: (f64, f64),
+    velocity: (f64, f64)
 }
